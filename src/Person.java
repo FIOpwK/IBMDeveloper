b80272eeb2b2a8aa1b45ee2f7b4a36cae7998c9c
fiopwk <flopwiki@gmail.com>
2023-06-21 18:15:09 -0400

    Package: creating a java project

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+package com.developer.ibm;
+
+public class Person {
+	private String name;
+	private int age;
+	private int height;
+	private int weight;
+	private String eyeColor;
+	private String gender;
+	
+	
+}
